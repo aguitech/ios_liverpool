44538a07531b2f1d5c73b29258c81fbddb18a39c
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2020-07-30 13:26:03 -0500

    Preparando clase de producto

---
+++ LiverpooliOS/LiverpooliOS/Producto.swift
@@ -0,0 +1,23 @@
+//
+//  Producto.swift
+//  LiverpooliOS
+//
+//  Created by Hector Aguilar on 7/30/20.
+//  Copyright © 2020 Hector Aguilar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Producto{
+    var image: UIImage
+    var title: String
+    
+    init(image: UIImage, title: String){
+        self.image = image
+        self.title = title
+        
+    }
+    
+}
+
---
+++ LiverpooliOS/LiverpooliOS/ProductoCell.swift
@@ -0,0 +1,35 @@
+//
+//  ProductoCell.swift
+//  LiverpooliOS
+//
+//  Created by Hector Aguilar on 7/30/20.
+//  Copyright © 2020 Hector Aguilar. All rights reserved.
+//
+
+import UIKit
+class ProductoCell: UITableViewCell{
+    
+    //@IBOutlet weak var blogTitleLabel: UILabel!
+    
+    //@IBOutlet weak var blogLikesLabel: UILabel!
+    //@IBOutlet weak var blogFechaLabel: UILabel!
+    
+    //@IBOutlet weak var blogImageView: UIImageView!
+    /*
+    func setPublicacion(publicacion: Publicacion){
+        
+        blogTitleLabel.text = publicacion.title
+        
+        blogLikesLabel.text = publicacion.likes
+        
+        blogFechaLabel.text = publicacion.fecha
+        
+        blogImageView.image = publicacion.image
+        
+        
+        
+        
+    }
+    */
+}
+
