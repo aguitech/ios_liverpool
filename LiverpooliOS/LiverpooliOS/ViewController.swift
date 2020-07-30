a7cede65f223e2675fd45d8a8923e0e4774408f8
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2020-07-30 15:29:37 -0500

    Preparando para mapeo de información

--- LiverpooliOS/LiverpooliOS/ViewController.swift
+++ LiverpooliOS/LiverpooliOS/ViewController.swift
@@ -24,7 +24,6 @@ class ViewController: UIViewController {
       var misproductos: [String] = []
       var idsproductos: [String] = []
     
-    //var mascotas: [Mascota] = []
     var productos: [Producto] = []
     
     @IBAction func buscar(_ sender: UIButton) {
@@ -32,14 +31,6 @@ class ViewController: UIViewController {
         let url = URL(string: "https://shoppapp.liverpool.com.mx/appclienteservices/services/v3/plp?force-plp=true&search-string=playera&page-number=1&number-of-items-per-page=20")
         var request : URLRequest = URLRequest(url: url!)
         request.httpMethod = "GET"
-        
-        /*
-        let postString = "id_mascota=12&id_usuario=\(idUsuario)&buscar=\(String(txtBuscar.text!))&sexo=\(String(sexoInput.text!))&peso=\(String(pesoInput.text!))&edad=\(String(edadInput.text!))&altura=\(String(alturaInput.text!))&raza=\(String(razaInput.text!))&esterilizado=\(String(esterilizadoInput.text!))"
-        
-        print(postString)
-        
-        request.httpBody = postString.data(using: .utf8)
-        */
 
         let dataTask = URLSession.shared.dataTask(with: request) {
             data,response,error in
@@ -54,16 +45,6 @@ class ViewController: UIViewController {
                     DispatchQueue.main.async() {
                         print("probando")
                         
-                        /*
-                        let storyBoard: UIStoryboard = UIStoryboard(name: "Forms", bundle: nil)
-                        let Pet = storyBoard.instantiateViewController(withIdentifier: "MyPets") as! MyPets
-                        Pet.idUsuario = self.idUsuario
-                        self.present(Pet, animated: false, completion: nil)
-                        
-                        */
-                        
-                      //completion(NSArray(object: teamResult), Int(teamInput.text!)!)
-                        //completion(NSArray(object: teamResult)!)
                     }
                     
                 }
@@ -86,15 +67,9 @@ class ViewController: UIViewController {
       
       var tempProductos: [Producto] = []
       
-      //http://pekits.info/json/mascotas.php?id_usuario=2
-      
-      //let url = URL(string: "https://api-aguitech.casidios.com/api_login.php?aguitech=backend")
-      //https://shoppapp.liverpool.com.mx/appclienteservices/services/v3/plp?force-plp=true&search-string=playera&page-number=1&number-of-items-per-page=20
       //let url = URL(string: "https://pekits.info/json/mascotas.php?id_usuario=\(idUsuario))")
       let url = URL(string: "https://shoppapp.liverpool.com.mx/appclienteservices/services/v3/plp?force-plp=true&search-string=playera&page-number=1&number-of-items-per-page=20")
       
-        //let url = URL(string: "https://pekits.info/json/mascotas_ios.php?id_usuario=2")
-      //let url = URL(string: "https://pekits.info/json/alta_mascota_basico.php")
       var request : URLRequest = URLRequest(url: url!)
       request.httpMethod = "GET"
       
@@ -110,43 +85,49 @@ class ViewController: UIViewController {
         
         
         if error != nil {
-          print("some error occured")
+          print("ocurrio un error)
         } else {
           
-          print("jalo")
           if let urlContent =  data {
             
             do{
               let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
               
               // I would not recommend to use NSDictionary, try using Swift types instead
-              guard let newValue = jsonResult as? [String: Any] else {
+              guard let jsonValue = jsonResult as? [String: Any] else {
                 print("invalid format")
                 return
               }
-              
-              // Check for the weather parameter as an array of dictionaries and than excess the first array's description
-              if let weather = newValue["data"] as? [[String: Any]], let description = weather.first?["nombre"] as? String {
+                print(jsonValue)
+                
+                print("Resultado")
+                
+                print(jsonValue["plpResults"])
+                
+                print("RecordsX")
+                
+                print(jsonValue["plpResults"])
+                
+                let resultadosPrevios = jsonValue["plpResults"];
+                
+                
+            if let resultado = jsonValue["plpResults"] as? [[String: Any]], let description = resultado.first?["nombre"] as? String {
+                
+                print("Hola")
+                
                 print(description)
                 
-                print(weather.count)
+                print(resultado.count)
                 
-                for item in weather{
+                for item in resultado{
                   print(item)
                   
-                  //self.results = item["edad"] as! [Array]
-                  //self.results = item["edad"] as! NSArray
                   
-                  print(item["edad"]!)
-                  
-                  //let pex = Mascota(image: UIImage(imageLiteralResourceName: "icono_perro"), title: item["nombre"]! as! String)
-                  
-                  //self.mismascotas.append("Tom")
                   self.misproductos.append(item["nombre"]! as! String)
               
-                  self.idsproductos.append(item["id_mascota"]! as! String)
+                  self.idsproductos.append(item["id_producto"]! as! String)
                   //let pex = Mascota(image: UIImage(imageLiteralResourceName: "icono_perro"), title: "Tom")
-                  var pex = Producto(imagen: UIImage(imageLiteralResourceName: "icono_perro"), nombre: item["nombre"]! as! String, nombre: item["descripcion"]! as! String, nombre: item["precio"]! as! String)
+                  var pex = Producto(imagen: UIImage(imageLiteralResourceName: "url"), nombre: item["nombre"]! as! String, descripcion: item["descripcion"]! as! String, precio: item["precio"]! as! String)
                   
                   tempProductos.append(pex)
                   
