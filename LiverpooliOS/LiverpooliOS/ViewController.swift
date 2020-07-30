ad0160561d47f13b8f443a846ba4525ab04f5232
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2020-07-30 12:37:22 -0500

    Creación de App Liverpool por medio de Storyboard

---
+++ LiverpooliOS/LiverpooliOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiverpooliOS
+//
+//  Created by Hector Aguilar on 7/30/20.
+//  Copyright © 2020 Hector Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LiverpooliOS/LiverpooliOSTests/LiverpooliOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  LiverpooliOSTests.swift
+//  LiverpooliOSTests
+//
+//  Created by Hector Aguilar on 7/30/20.
+//  Copyright © 2020 Hector Aguilar. All rights reserved.
+//
+
+import XCTest
+@testable import LiverpooliOS
+
+class LiverpooliOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
